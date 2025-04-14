de4164f47c64f7e756fae283f3251cffeba535cf
qqKazakov <leap74rus@gmail.com>
2025-04-14 12:53:48 +0300

    fix: serializationGroups

--- src/Cart/Serializer/SerializationGroups.php
+++ src/Cart/Serializer/SerializationGroups.php
@@ -6,7 +6,7 @@ namespace App\Cart\Serializer;
 
 final class SerializationGroups
 {
-    public const CART_READ       = 'cart:read';
-    public const CART_WRITE      = 'cart:write';
-    public const CART_ITEMS_READ = 'cart:items_read';
+    public const string CART_READ       = 'cart:read';
+    public const string CART_WRITE      = 'cart:write';
+    public const string CART_ITEMS_READ = 'cart:items_read';
 }
